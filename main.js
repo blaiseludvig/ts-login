e75ead54f5b77d4d46a1709f13c5d63d1d596dab
blaiseludvig <blaiseludvig@users.noreply.github.com>
2022-10-13 15:33:24 +0000

    deploy: 0467a1243c36e281ecd5edd4f47d578a8517ca71

--- main.js
+++ main.js
@@ -16,7 +16,7 @@
   \***********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://change_me/./src/style.css?");
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://ts-login/./src/style.css?");
 
 /***/ }),
 
@@ -24,9 +24,19 @@ eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extr
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.css */ \"./src/style.css\");\n\n// import { animateCSS } from \"./animatecss\";\n\nlet element = document.createElement('h1');\nelement.textContent = \"Ha ezt látod, akkor működik!\"\ndocument.body.append(element);\n\n\n//# sourceURL=webpack://change_me/./src/index.js?");
+eval("\nObject.defineProperty(exports, \"__esModule\", ({ value: true }));\n__webpack_require__(/*! ./style.css */ \"./src/style.css\");\nconst util_1 = __webpack_require__(/*! ./util */ \"./src/util.js\");\n// import { animateCSS } from \"./animatecss\";\nlet input_username = document.querySelector(\"#input_username\");\ninput_username.addEventListener(\"change\", () => {\n    if ((0, util_1.isValidUsername)(input_username.value).isValid == false) {\n        alert((0, util_1.isValidUsername)(input_username.value).message);\n    }\n});\n\n\n//# sourceURL=webpack://ts-login/./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/util.js":
+/*!*********************!*\
+  !*** ./src/util.js ***!
+  \*********************/
+/***/ ((__unused_webpack_module, exports) => {
+
+eval("\nObject.defineProperty(exports, \"__esModule\", ({ value: true }));\nexports.isValidUsername = void 0;\nfunction isValidUsername(username) {\n    // reject if username is empty\n    if (username.length == 0)\n        return { isValid: false, message: \"Válassz egy felhasználnevet!\" };\n    // reject if username is short than 6 or longer than 30 characters\n    if (username.length < 6 || username.length > 30)\n        return { isValid: false, message: \"Válassz egy 6-30 karakter hosszú felhasználnevet!\" };\n    // reject if username is a reserved alias\n    if ((/abuse|postmaster/gi).test(username))\n        return { isValid: false, message: 'A felhasználónév nem lehet \"abuse\" vagy \"postmaster\"' };\n    // reject if username contains two consecutive dots\n    if ((/\\.\\./gi).test(username))\n        return { isValid: false, message: 'A felhasználónévben nem lehet két egymás melletti pont!' };\n    // reject if username contains non-alphanumeric characters\n    if ((/^(([a-z]|[0-9]|[\\.]))*$/gi).test(username) == false)\n        return { isValid: false, message: 'A felhasználónév csak latin betűket és számokat tartalmazhat!' };\n    // reject if username starts or ends with dot\n    if ((/(^\\.)|(\\.$)/gi).test(username))\n        return { isValid: false, message: 'A felhasználónév nem kezdődhet vagy végződhet ponttal!' };\n    return { isValid: true, message: null };\n}\nexports.isValidUsername = isValidUsername;\n\n\n//# sourceURL=webpack://ts-login/./src/util.js?");
 
 /***/ })
 
